c22fc17b767613e7a1e36a3e2eec2b70646f4274
Alvintxh <alvin2003tian@outlook.com>
2025-01-24 19:49:10 +0800

    date 25-01-24
    change some error

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,7 +4,9 @@ return {
 		ft = { "markdown" },
 		build = "yarn install",
 		config = function()
-			vim.g.instant_markdown_autostart = 0
+			vim.g.instant_markdown_autostart = 1
+			vim.g.instant_markdown_mathjax = 1
+			vim.g.instant_markdown_mermaid = 1
 		end,
 	},
 }
